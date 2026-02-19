a474c8b1e7154336a61237a017340f3bc51e0b54
anisaags15 <anisaags024@gmail.com>
2026-02-19 12:21:52 +0700

    add admin seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -11,13 +11,14 @@ class AdminSeeder extends Seeder
     public function run(): void
     {
         // Ini data adminteh yang tadi hilang di laptop temenmu
-        User::create([
-            'name' => 'Admin Pusat',
-            'username' => 'admin',
-            'email' => 'adminteh@gmail.com',
-            'password' => Hash::make('password'), // Nanti loginnya pake password ini
-            'role' => 'admin',
-            'email_verified_at' => now(),
-        ]);
+User::create([
+    'name' => 'Admin Pusat',
+    'username' => 'admin',
+    'email' => 'adminteh@gmail.com',
+    'no_hp' => '08123456789', 
+    'password' => Hash::make('admin123'), // SUDAH DIGANTI JADI admin123
+    'role' => 'admin',
+    'email_verified_at' => now(),
+]);
     }
 }
